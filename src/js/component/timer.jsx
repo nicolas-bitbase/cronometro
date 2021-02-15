9c8e3d29770a74a45c5aab4f16605cadfbdb58ab
nicolas-bitbase <nicolas@bitbase.es>
2021-02-15 10:35:18 +0000

    cambios

--- src/js/component/timer.jsx
+++ src/js/component/timer.jsx
@@ -2,14 +2,19 @@ import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 export default function Counter() {
-	const [contador, setContador] = useState(0);
+	const [contador1, setContador1] = useState(0);
+	const [contador2, setContador2] = useState(0);
+	const [contador3, setContador3] = useState(0);
 
 	useEffect(() => {
 		let cnt = 0;
 		setInterval(() => {
-			setContador(cnt++);
-			console.log(cnt);
-		}, 1000);
+			setContador3(Math.floor(cnt / 3600));
+			setContador2(Math.floor(cnt / 60));
+			setContador1(Math.floor(cnt / 1));
+
+			cnt++;
+		}, 10);
 	}, []);
 
 	return (
@@ -17,9 +22,9 @@ export default function Counter() {
 			<div className="primero"> </div>
 			<div className="segundo"></div>
 			<div className="tercero"></div>
-			<div className="cuarto"></div>
-			<div className="quinto"></div>
-			<div className="sexto">{contador}</div>
+			<div className="cuarto">{contador3 % 24}</div>
+			<div className="quinto">{contador2 % 60}</div>
+			<div className="sexto">{contador1 % 60}</div>
 		</div>
 	);
 }
